fa47b2168c386cb297cf72095d633c10edea4435
skidev101 <skidev101@gmail.com>
2025-10-11 01:50:35 +0100

    feat: created sanity config files && migrated codebase to pnpm

---
+++ app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "easymde/dist/easymde.min.css"
+
+export default function StudioLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
